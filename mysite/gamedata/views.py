6f0997c42807c512218a682086c41114b10a4fec
furlongi <ivanfurlong@gmail.com>
2021-08-21 21:48:30 -0700

    Switched back to IGDB API
    Also updated requirements.txt

--- mysite/gamedata/views.py
+++ mysite/gamedata/views.py
@@ -18,28 +18,26 @@ def search(request):
     if request.method == 'GET':
         game_name = request.GET.get('name', None)
 
-        # IGDB API - No longer used
-        # api = SearchType()
-        # api.set_search(game_name)
-        # api.add_field('name')
-        # api.add_where('rating > 0')
-        # print(api)
-        # results = Credentials.request(api)
-        # print(results)
+        # IGDB API
+        api = SearchType()
+        api.set_search(game_name)
+        api.add_field('name')
+        api.add_where('rating > 0')
+        results = Credentials.request(api)
 
         # RAWG API
-        rawg = RawgSearch()
-        rawg.set_search(game_name)
-        rawg.set_length(10)
-        results = rawg.request()
+        # rawg = RawgSearch()
+        # rawg.set_search(game_name)
+        # rawg.set_length(10)
+        # results = rawg.request()
 
         # Add Id's if games are missing
-        add_new_titles_rawg(results)
+        add_new_titles(results)
 
         # Get ResultSet
         array = filter_array_json('id', results)
-        # rs = {'data': GameInfo.objects.filter(igdblink__igdb_id__in=array)}
-        rs = {'data': GameInfo.objects.filter(rawglink__rawg_id__in=array)}
+        rs = {'data': GameInfo.objects.filter(igdblink__igdb_id__in=array)}
+        # rs = {'data': GameInfo.objects.filter(rawglink__rawg_id__in=array)}
 
         # Create and Return HttpResponse
         serial = GameInfoContainerSerializer(rs)
